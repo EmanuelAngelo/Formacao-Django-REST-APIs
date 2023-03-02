148608ff8c89e6110487bf4897df37fe0708a23c
Emanuel Angelo <emanuelangelo@outlook.com.br>
2023-03-02 16:54:39 -0300

    primeiro curso da formacao

---
+++ 1API-comDjango3-rest-framework/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\emanuel.almeida\Documents\Projetos\Formação Django REST APis\1API-comDjango3-rest-framework\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
