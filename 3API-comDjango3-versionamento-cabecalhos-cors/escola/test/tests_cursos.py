a199298b297fc5005ecc6a595f6f90138569d3a8
Emanuel Angelo <emanuelangelo@outlook.com.br>
2023-03-09 09:43:32 -0300

    testes

--- 3API-comDjango3-versionamento-cabecalhos-cors/escola/test/tests_cursos.py
+++ 3API-comDjango3-versionamento-cabecalhos-cors/escola/test/tests_cursos.py
@@ -3,6 +3,8 @@ from escola.models import Curso
 from django.urls import reverse
 from rest_framework import status
 
+# manage.py test
+
 
 class CursosTestCase(APITestCase):
 
@@ -35,3 +37,16 @@ class CursosTestCase(APITestCase):
 
     def test_requisicao_delete_curso(self):
         """Teste para ficar requisicao DELETE: Deletando cursos"""
+        response = self.client.delete('/cursos/1/')
+        self.assertEquals(response.status_code,
+                          status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_requisicao_put_att_curso(self):
+        """Teste para ficar requisicao PUT: Atualizando cursos"""
+        data = {
+            'codigo_curso': 'CTT1',
+            'descricao': 'CURSOS ATT',
+            'nivel': 'I'
+        }
+        response = self.client.put('/cursos/1/', data=data)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
