60372c5a597aca20978b20e044fa6c4ef58dfeaa
Emanuel Angelo <emanuelangelo@outlook.com.br>
2023-03-09 09:58:48 -0300

    conclusao com honeypot

--- 3API-comDjango3-versionamento-cabecalhos-cors/escola/models.py
+++ 3API-comDjango3-versionamento-cabecalhos-cors/escola/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 from django.db import models
 
-
 class Aluno(models.Model):
     nome = models.CharField(max_length=30)
     rg = models.CharField(max_length=9)
@@ -14,7 +13,6 @@ class Aluno(models.Model):
     def __str__(self):
         return self.nome
 
-
 class Curso(models.Model):
     NIVEL = (
         ('B', 'Básico'),
@@ -23,12 +21,10 @@ class Curso(models.Model):
     )
     codigo_curso = models.CharField(max_length=10)
     descricao = models.CharField(max_length=100)
-    nivel = models.CharField(max_length=1, choices=NIVEL,
-                             blank=False, null=False, default='B')
+    nivel = models.CharField(max_length=1, choices=NIVEL, blank=False, null=False,default='B')
 
     def __str__(self):
         return self.descricao
-
 
 class Matricula(models.Model):
     PERIODO = (
@@ -38,5 +34,5 @@ class Matricula(models.Model):
     )
     aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
     curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
-    periodo = models.CharField(
-        max_length=1, choices=PERIODO, blank=False, null=False, default='M')
+    periodo = models.CharField(max_length=1, choices=PERIODO, blank=False, null=False,default='M')
+
--- 3API-comDjango3-versionamento-cabecalhos-cors/setup/settings.py
+++ 3API-comDjango3-versionamento-cabecalhos-cors/setup/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'we2q@@2p30+=^#(xbgl7e@3+r2dt+zmf#us2m$ix=q&n+=y-z$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['localhost']
 
 # Application definition
 
@@ -37,10 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'rest_framework',
     'escola',
     'corsheaders',
+    'admin_honeypot',
 ]
 
 MIDDLEWARE = [
@@ -91,18 +90,18 @@ DATABASES = {
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    # {
-    #     'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    # },
-    # {
-    #     'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    # },
-    # {
-    #     'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    # },
-    # {
-    #     'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    # },
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
 ]
 
 
@@ -119,50 +118,53 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 MEDIA_URL = '/media/'
 
 REST_FRAMEWORK = {
     'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.QueryParameterVersioning',
-    # 'DEFAULT_PERMISSION_CLASSES': [
+    # 'DEFAULT_PERMISSION_CLASSES':[
     #     'rest_framework.permissions.IsAuthenticated',
     #     'rest_framework.permissions.DjangoModelPermissions'
     # ],
-    # 'DEFAULT_AUTHENTICATION_CLASSES': [
+    # 'DEFAULT_AUTHENTICATION_CLASSES':[
     #     'rest_framework.authentication.BasicAuthentication',
     # ],
     'DEFAULT_THROTTLE_CLASSES': [
         'rest_framework.throttling.AnonRateThrottle',
-        # 'rest_framework.throttling.UserRateThrottle',
     ],
     'DEFAULT_THROTTLE_RATES': {
         'anon': '100/day',
-        # 'user': '1000/day',
-    }
+    },
+    # 'DEFAULT_PARSER_CLASSES': [
+    #     'rest_framework.parsers.JSONParser',
+    #     # 'rest_framework_xml.parsers.XMLParser',
+    # ],
+    # 'DEFAULT_RENDERER_CLASSES': [
+    #     'rest_framework.renderers.JSONRenderer',
+    #     # 'rest_framework_xml.renderers.XMLRenderer',
+    # ],
 }
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
+    "http://localhost:3000",
 ]
 
 # CACHES = {
-# """Usando Redis"""
-#     'default': {
-#         'BACKEND': 'django_redis.cache.RedisCache',
-#         'LOCATION': 'redis://enderençoaqui',
-#         'OPTIONS': {
-#             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": "redis://127.0.0.1:6379/1",
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
 #         }
 #     }
 # }
 
-# SESSION_ENGINE = 'django.contrib.sessions.backend.cache'
-# SESSION_CACHE_ALIAS = 'default'
+# SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+# SESSION_CACHE_ALIAS = "default"
 
 LOCALE_PATHS = (
     os.path.join(BASE_DIR, 'locale/'),
